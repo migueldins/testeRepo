4715326f491491465fbc5a71a9725f701196ad4e
Miguel Dinis <miguel.dinis@TIMWE.COM>
2015-11-30 11:16:19 +0000

    greetings

---
+++ src/Greeter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Greeter;
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Greeter
+{
+    public function greet()
+    {
+        return "hello world";
+    }
+}
+
